ae6567e0a7ba437f7478340bb311644e9eeeba1c
diwal123 <diwalpyakurel@gmail.com>
2019-04-11 18:50:28 -0500

    My Change-DiwalPya

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -1,47 +1,82 @@
 var db = require("../models");
 
-module.exports = function (app) {
+module.exports = function(app) {
   // Get all examples
-  app.get("/api/transactions/:id", function (req, res) {
-    db.Transaction.findAll(
-      {
-        where: {
-          user_ID: req.params.id
-        }
-      }).then(function (results) {
-        res.json(results);
-      });
+  app.get("/api/transactions/:id", function(req, res) {
+    console.log(req.params.id);
+    db.Transaction.findAll({
+      where: {
+        user_ID: req.params.id
+      }
+    }).then(function(results) {
+      res.json(results);
+    });
   });
 
-  app.get("/api/users", function (req, res) {
-    db.User.findAll().then(function (results) {
+  app.get("/api/transaction-deposit/:id", function(req, res) {
+    console.log(req.params.id);
+    db.Transaction.findAll({
+      where: {
+        type: "Deposit",
+        user_ID: req.params.id
+      }
+    }).then(function(results) {
+      res.json(results);
+    });
+  });
+
+  app.get("/api/transaction-withdrawl/:id", function(req, res) {
+    console.log(req.params.id);
+    db.Transaction.findAll({
+      where: {
+        type: "Withdrawl",
+        user_ID: req.params.id
+      }
+    }).then(function(results) {
+      res.json(results);
+    });
+  });
+
+  app.get("/api/users", function(req, res) {
+    db.User.findAll().then(function(results) {
       res.json(results);
     });
   });
 
   // Gets all transactions for admin
-  app.get("/api/all-transactions", function (req, res) {
-    db.transaction.findAll().then(function (results) {
+  app.get("/api/all-transactions", function(req, res) {
+    db.Transaction.findAll().then(function(results) {
+      res.json(results);
+    });
+  });
+
+ 
+  
+  app.get("/api/budget", function(req, res) {
+    console.log(req.params.id);
+    db.Transaction.findAll().then(function(results) {
       res.json(results);
     });
   });
 
 
+
+  
+
   // post transactions
-  app.post("/api/transactions", function (req, res) {
+  app.post("/api/transactions", function(req, res) {
     console.log("req.body");
     db.Transaction.create({
       user_ID: req.body.user_ID,
       type: req.body.type,
       amount: req.body.amount,
       categories: req.body.categories
-    }).then(function (results) {
-      res.json(results)
-    })
-
+    }).then(function(results) {
+      res.json(results);
+    });
   });
 
-  app.post("/api/users", function (req, res) {
+  app.post("/api/users", function(req, res) {
     console.log(req.body);
     db.User.create({
       firstName: req.body.firstName,
@@ -50,12 +85,11 @@ module.exports = function (app) {
       username: req.body.username,
       password: req.body.password,
       deposit: req.body.deposit
-    }).then(function (results) {
-      res.json(results)
-    })
+    }).then(function(results) {
+      res.json(results);
+    });
   });
-}
-
+};
 
 /*
 // Delete an example by id
@@ -66,6 +100,3 @@ app.delete("/api/examples/:id", function(req, res) {
 });
 };
 */
-
-
-
