366e5df3589d6892e11647a08639b7f02d09d191
diwal123 <diwalpyakurel@gmail.com>
2019-04-12 08:14:51 -0500

    Change

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -50,18 +50,12 @@ module.exports = function(app) {
     });
   });
 
- 
-  
   app.get("/api/budget", function(req, res) {
     console.log(req.params.id);
     db.Transaction.findAll().then(function(results) {
       res.json(results);
     });
   });
-
-
-
-  
 
   // post transactions
   app.post("/api/transactions", function(req, res) {
@@ -89,7 +83,37 @@ module.exports = function(app) {
       res.json(results);
     });
   });
+
+  app.get("/api/budget/:id", function(req, res) {
+    console.log(req.params.id);
+    let sequelize = db.sequelize;
+    let Op = db.Sequelize.Op;
+
+    db.Transaction.findAll({
+    attributes: [[sequelize.fn('SUM', sequelize.col('amount')), 'amount'],['categories', 'categories']],
+    where: {[Op.and]: [{user_ID: {[Op.eq]: req.params.id}}, {type: {[Op.eq]: "Withdrawal"}}]},
+    group: ['categories'],
+    }).then(function(results) {
+      res.json(results);
+    });
+  });
+
+  app.get("/api/budget-other", function(req, res) {
+    console.log(req.params.id);
+    let sequelize = db.sequelize;
+    let Op = db.Sequelize.Op;
+
+    db.Transaction.findAll({
+    attributes: [[sequelize.fn('SUM', sequelize.col('amount')), 'amount'],['categories', 'categories']],
+    where: {[Op.and]: [{type: {[Op.eq]: "Withdrawal"}}]},
+    group: ['categories'],
+    }).then(function(results) {
+      res.json(results);
+    });
+  });
 };
+
+
 
 /*
 // Delete an example by id
